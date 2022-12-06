aeffb2746842e811f2b9549401175b41a9f750c8
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-06 09:51:52 +0300

    add book storage

---
+++ modules/bookStorage.js
@@ -0,0 +1,15 @@
+export class BookStorage {
+  addBook = (newBook) => {
+    let books = JSON.parse(localStorage.getItem('allEntries'));
+    if (books == null) books = [];
+    localStorage.setItem('entry', JSON.stringify(newBook));
+    books.push(newBook);
+    localStorage.setItem('allEntries', JSON.stringify(books));
+  }
+
+  removeBook = (index) => {
+    const books = JSON.parse(localStorage.getItem('allEntries'));
+    books.splice(index, 1);
+    localStorage.setItem('allEntries', JSON.stringify(books));
+  }
+}
